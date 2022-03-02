b54dc1358a02f97d39d8bf915f6ba8579397ac4f
田口 優宝 <taguchimasataka@MacBook-Air-2.local>
2022-03-02 16:40:02 +0900

    my second commit

--- app_login.py
+++ app_login.py
@@ -1,11 +1,36 @@
 from flask import session, redirect
-
+import sqlite3
 #ログインに使うユーザ名とパスワード
-USERLIST = {
-    'taro': 'aaa',
-    'jiro': 'bbb',
-    'sabu': 'ccc',
-}
+#USERLIST = {
+#    'taro': 'aaa',
+#    'jiro': 'bbb',
+#    'sabu': 'ccc',
+#}
+
+dbname = 'ROOM.db'
+conn = sqlite3.connect(dbname)
+cur = conn.cursor()
+
+# 参考:https://qiita.com/wiwi7373/items/7d47decf85a77454074d
+# dict_factoryの定義
+def dict_factory(cursor, row):
+   d = {}
+   for idx, col in enumerate(cursor.description):
+       d[col[0]] = row[idx]
+   return d
+
+
+# row_factoryの変更(dict_factoryに変更)
+conn.row_factory = dict_factory
+
+# SELECT文の発行
+cur.execute('SELECT * FROM userlist')
+
+       
+conn.row_factory = dict_factory
+# レコードを1行取得
+USERLIST = cur.fetchall()
+
 
 #ログインしているか調べる
 def is_login():
@@ -30,3 +55,6 @@ def try_logout():
 def get_user():
     if is_login(): return session['login']
     return 'not login'
+
+cur.close()
+conn.close()
